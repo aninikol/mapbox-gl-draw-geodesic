4b5a63ee327257b735428b344e6ec62b0d3536c6
AnysiaNikolopoulou <aninikol@gmail.com>
2023-07-25 12:30:52 +0300

    Try to fix errors

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,16 @@
-if (typeof MapboxDraw.constants !== 'undefined') {
+/*if (typeof MapboxDraw.constants !== 'undefined') {
     const drawConstants = await import('@mapbox/mapbox-gl-draw/src/constants');
     export const { cursors, geojsonTypes, events, meta, activeStates } = drawConstants;
 } else {
     export const  { cursors, geojsonTypes, events, meta, activeStates } = MapboxDraw.constants;
-}
+}*/
+console.log(MapboxDraw.modes);
+let originalModes = MapboxDraw.modes;
+(async () => {
+    //  Modes = await import(MapboxDraw.constants);
+    export const { cursors, geojsonTypes, events, meta, activeStates } = MapboxDraw.constants;
+}) ();
+
 export const modes = {
     ...originalModes,
     DRAW_CIRCLE: 'draw_circle'
